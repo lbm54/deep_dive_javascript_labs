f409de78a5cf32a3c934ef73a3295642f92e9180
Lee Martin <lee.b.martin@gmail.com>
2018-06-04 15:40:53 -0700

    context clues

---
+++ script.js
@@ -0,0 +1,20 @@
+var FRIEND_NAMES = ['Adam', 'Barbara', 'Charlie', 'Davis', 'Eric'];
+var LOCATION_NAMES = ['Study', 'Dining Room', 'Billiard Room', 'Library', 'Conservatory', 'Kitchen', 'Foyer', 'Hall', 'Lounge', 'Bathroom'];
+var WEAPON_NAMES = ['Acid', 'Axe', 'Battleaxe', 'Candlestick', 'Chainsaw', 'Cinderblock', 'Doberman', 'Jack', 'Knife', 'Lead Pipe', 'Laser', 'Machine Gun', 'Machete', 'Poison', 'Revolver', 'Rifle', 'Rope', 'Sword', 'Shotgun', 'Wrench']
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (var i = 0; i < 100; i++) {
+        var h3 = document.createElement('h3');
+        var h3Text = `Accusation${i}`;
+        var h3TextNode = document.createTextNode(h3Text);
+        h3.appendChild(h3TextNode);
+        h3.addEventListener('click', handleAccusation(i));
+        document.body.appendChild(h3);
+    }
+
+    function handleAccusation(i) {
+        return function() {
+            alert(`Accusation${i}: I accuse ${FRIEND_NAMES[i % 5]}, with the ${WEAPON_NAMES[i % 10]} in the ${LOCATION_NAMES[i % 20]}!`);
+        }
+    }
+})
