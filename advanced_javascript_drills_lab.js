b3a0ee2c00975947d5b2d4c2d63d2f346d53ec51
Lee Martin <lee.b.martin@gmail.com>
2018-06-04 13:11:16 -0700

    advanced_javascript_drills_lab

---
+++ advanced_javascript_drills_lab.js
@@ -0,0 +1,66 @@
+// name = "Lee Martin";
+// var name;
+// console.log(name);
+
+// name = "Lee Martin";
+// let name;
+// console.log(name);
+
+// name = "Lee Martin";
+// var name;
+// console.log(name);
+
+// setName();
+// function setName() {
+//     var name = 'Covalence';
+//     console.log(name);
+// }
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['apple', 'tomato', 'banana'];
+// // let favFruit;
+// let favFruit = fruits[2];
+// function printFruits() {
+//     favFruit = fruits[2];
+//     console.log(fruits[0]);
+// }
+
+// function printFavFruit() {
+//     console.log(favFruit);
+// }
+
+// printFruits();
+// printFavFruit();
+
+// function printFruits() {
+//     let favFruit = fruits[2];
+//     console.log(fruits[0]);
+
+//     function printFavFruit() {
+//         console.log(favFruit);
+//     }
+// }
+
+// printFruits();
+// printFavFruit();
+
+// let leastFav = favFruit;
+// console.log(leastFav);
+
+// someFunc();
+// function someFunc() {
+//     console.log('Hello, Lee');
+// }
+
+someFunc();
+let someFunc = function() {
+    alert('hey');
+}
