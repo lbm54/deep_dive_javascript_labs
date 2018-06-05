7e2b218ada08588e7516f396604834fc1ed743da
Lee Martin <lee.b.martin@gmail.com>
2018-06-05 09:38:39 -0700

    fixed array index issue

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var LOCATION_NAMES = ['Study', 'Dining Room', 'Billiard Room', 'Library', 'Conse
 var WEAPON_NAMES = ['Acid', 'Axe', 'Battleaxe', 'Candlestick', 'Chainsaw', 'Cinderblock', 'Doberman', 'Jack', 'Knife', 'Lead Pipe', 'Laser', 'Machine Gun', 'Machete', 'Poison', 'Revolver', 'Rifle', 'Rope', 'Sword', 'Shotgun', 'Wrench']
 
 document.addEventListener("DOMContentLoaded", function() {
-    for (var i = 1; i < 101; i++) {
+    for (var i = 0; i < 100; i++) {
         var h3 = document.createElement('h3');
-        var h3Text = `Accusation${i}`;
+        var h3Text = `Accusation${i + 1}`;
         var h3TextNode = document.createTextNode(h3Text);
         h3.appendChild(h3TextNode);
         h3.addEventListener('click', handleAccusation(i));
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function handleAccusation(i) {
         return function() {
-            alert(`Accusation${i}: I accuse ${FRIEND_NAMES[i % 5]}, with the ${WEAPON_NAMES[i % 20]} in the ${LOCATION_NAMES[i % 10]}!`);
+            alert(`Accusation${i + 1}: I accuse ${FRIEND_NAMES[i % 5]}, with the ${WEAPON_NAMES[i % 20]} in the ${LOCATION_NAMES[i % 10]}!`);
         }
     }
 })
